9c9441b4ec7196eefa16b98af81ddf459a18a7fc
moahammadApon <mohammadapon11.com>
2024-01-07 23:37:06 +0600

    padding top bug fixed

--- src/components/shared/gap's/PaddingTop.jsx
+++ src/components/shared/gap's/PaddingTop.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PaddingTop = () => {
-  return <div className="lg:h-[140px] md:h-[140px] xm:h-[130px]"></div>;
+  return <div className="lg:h-[140px] md:h-[140px] xxs:h-[200px]"></div>;
 };
 
 export default PaddingTop;
