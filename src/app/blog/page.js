668269e398ba3b6f116e1c4c28d7b5390c42aa8c
moahammadApon <mohammadapon11.com>
2024-01-11 18:29:38 +0600

    done push

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,4 @@
-import SubscribeBox from "@/components/shared/SubscribeBox";
-import TrendingBlogs from "@/components/shared/TrendingBlogs";
 import PaddingTop from "@/components/shared/gap's/PaddingTop";
-import Meeting from "@/components/shared/meeting";
 import React from "react";
 
 const BlogPage = () => {
